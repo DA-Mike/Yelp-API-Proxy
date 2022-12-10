ea14470fab430f2981146f6693ae95cfef30f433
Mike Rugh <mike.rugh@gmail.com>
2022-12-10 10:26:41 -0800

    adds cors mode

--- app_proxy.js
+++ app_proxy.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
+const cors = require("cors");
 
 app.get('*', (req, res) => {
 
     const AuthStr = "Bearer ".concat(req.query.apiKey);
+
+    app.use(cors);
 
     axios.get('https://api.yelp.com/v3/businesses/search?term=' + req.query.term + "&location=" + req.query.location + "&radius=" + req.query.radius + "&limit=30",
     {headers: { Authorization: AuthStr, dataType: JSON, mode: 'cors'}}
