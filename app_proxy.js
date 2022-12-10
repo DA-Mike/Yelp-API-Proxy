50b2bd4716066dbee4ec93e2e52661f0bb42aed8
Mike Rugh <mike.rugh@gmail.com>
2022-12-10 10:51:16 -0800

    makes some changes

--- app_proxy.js
+++ app_proxy.js
@@ -3,24 +3,28 @@ const app = express();
 const axios = require("axios");
 const cors = require("cors");
 
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+
 app.get('*', (req, res) => {
 
     const AuthStr = "Bearer ".concat(req.query.apiKey);
-
-    app.use(cors);
 
     axios.get('https://api.yelp.com/v3/businesses/search?term=' + req.query.term + "&location=" + req.query.location + "&radius=" + req.query.radius + "&limit=30",
     {headers: { Authorization: AuthStr, dataType: JSON, mode: 'cors', "Access-Control-Allow-Origin": "*"}}
     )
     .then(response => {
-        res.header('Access-Control-Allow-Origin', 'https://da-mike.github.io/Bar-Hopper/');
+        res.header('Access-Control-Allow-Origin', '*');
         res.send(response.data);
     })
     .catch(error => {
-        res.header('Access-Control-Allow-Origin', 'https://da-mike.github.io/Bar-Hopper/');
+        res.header('Access-Control-Allow-Origin', '*');
         res.send(error.code);
         res.send(error);
     })
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function () {
+    console.log(`Listening on port ${PORT}`);
+});
